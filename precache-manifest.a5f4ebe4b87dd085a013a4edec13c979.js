2909dd529b25b8e9e2c062bab778700b05ac97a9
hnrneha <hnrneha123@gmail.com>
2023-02-05 17:52:05 +0530

    Updates

---
+++ precache-manifest.a5f4ebe4b87dd085a013a4edec13c979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da5b9ddbd80e688439f",
+    "url": "/bday_wishes_reactjs/static/css/main.e21b9693.chunk.css"
+  },
+  {
+    "revision": "1da5b9ddbd80e688439f",
+    "url": "/bday_wishes_reactjs/static/js/main.1da5b9dd.chunk.js"
+  },
+  {
+    "revision": "0460c8ab2c4c78dcf7df",
+    "url": "/bday_wishes_reactjs/static/js/1.0460c8ab.chunk.js"
+  },
+  {
+    "revision": "e0ec9cb9768584657374",
+    "url": "/bday_wishes_reactjs/static/js/runtime~main.e0ec9cb9.js"
+  },
+  {
+    "revision": "90118e04a5ab527cb88749db824d7b7b",
+    "url": "/bday_wishes_reactjs/static/media/githubLogo.90118e04.svg"
+  },
+  {
+    "revision": "a2e5556616a5afd51179892a87723060",
+    "url": "/bday_wishes_reactjs/index.html"
+  }
+];
